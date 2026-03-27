2b8bf2fd6bdf0ddc2509601502dd11eb2e721634
Walid El Kassem <walidelkassems@gmail.com>
2026-03-27 18:44:07 +0200

    revert liters calibration changes

--- js/config.js
+++ js/config.js
@@ -2,8 +2,7 @@
 window.API_BASE = "https://hallabio-tank-api-prod.walidelkassems.workers.dev/api";
 window.LOW_THRESHOLD = 5;
 window.AUTH_ENABLED = false;
-window.DISPLAY_LEVEL_SCALE = 0.8;
 window.TANK_CAPACITY_LITERS = {
-  tankA: 3125,
+  tankA: 3906.25,
   tankB: null
 };
