2d611be904db7580060c09ec2f8844ac66ba726a
Walid El Kassem <walidelkassems@gmail.com>
2026-03-27 14:35:34 +0200

    3 times

--- js/config.js
+++ js/config.js
@@ -3,6 +3,6 @@ window.API_BASE = "https://hallabio-tank-api-prod.walidelkassems.workers.dev/api
 window.LOW_THRESHOLD = 5;
 window.AUTH_ENABLED = false;
 window.TANK_CAPACITY_LITERS = {
-  tankA: 3906.25,
+  tankA: 3906.25 * 3,
   tankB: null
 };
